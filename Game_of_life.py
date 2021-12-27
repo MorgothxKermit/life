15fc099ecc1d361aa6eed8e0f6e2eced4376c652
Morgoth <242252@edu.p.lodz.pl>
2021-12-27 12:08:24 +0100

    Final edition, I hope, the animation works as intended

--- Game_of_life.py
+++ Game_of_life.py
@@ -4,25 +4,26 @@ import functions as life
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-plt.rc('figure', max_open_warning = 0)
+plt.rc('figure', max_open_warning=0)
 
 
 file_name = life.game_window()
 
 with open(file_name, 'r') as file:
     read_from_file = file.read()
-    #print(read_from_file)
+    # print(read_from_file)
 
 board = []
 
 read_from_file = read_from_file.split()
 for i in range(0, len(read_from_file)-1):
-    temp_list = ['X'  if j == 'X' else '.' for j in read_from_file[i]]
+    temp_list = [1 if j == 'X' else 0 for j in read_from_file[i]]
     board.append(temp_list)
-    
+
 board = np.array(board)
 
-game_board = board.copy() # I encountered a certain problem with the function, as if board was global, a quick fix was done
+# I encountered a certain problem with the function, as if board was global, a quick fix was done
+game_board = board.copy()
 
 """for i in range(0, board.shape[0]):
     print('\n')
@@ -30,18 +31,36 @@ game_board = board.copy() # I encountered a certain problem with the function, a
         print(board[i][j], end='')
 """
 #print('\n', game_board)
-while True:
-    game_board = life.check_neighbours(game_board) # main functionality, changes the board into next turn
-    print('\n'*10)
-    for i in range(0, board.shape[0]): # prints for now just to see if it works
-        print('\n')
-        for j in range(0, board.shape[1]): # board.shape to know the dimensions of the array
-            print(game_board[i][j], end='')
-    time.sleep(0.5)
-
 """while True:
+    # main functionality, changes the board into next turn
     game_board = life.check_neighbours(game_board)
-    fig, ax = plt.subplots()
-    img = ax.imshow(game_board)
-    ani = animation.FuncAnimation(fig, game_board, frames=10, interval=50, save_count=50)
-    plt.show"""
+    print('\n'*10)
+    # prints for now just to see if it works
+    for i in range(0, board.shape[0]):
+        print('\n')
+        # board.shape to know the dimensions of the array
+        for j in range(0, board.shape[1]):
+            if game_board[i][j] == 1:
+                print('X', end='')
+            else:
+                print('.', end='')
+    time.sleep(0.5)
+"""
+
+def animate(n):
+    global game_board
+    game_board = life.check_neighbours(game_board)
+    img.set_array(game_board)
+    return img
+
+game_board = life.check_neighbours(game_board)
+fig, ax = plt.subplots()
+img = ax.imshow(game_board)
+anim = animation.FuncAnimation(
+    fig, 
+    animate,
+    frames=30,
+    interval=500,
+    blit=False
+)
+plt.show()
--- functions.py
+++ functions.py
@@ -6,28 +6,25 @@ from PIL import Image
 
 
 def file_glider(file, root):
-        file.set('glider.txt')
-        root.destroy()
-
+    file.set('glider.txt')
+    root.destroy()
 
 
 def file_pulsar(file, root):
-        file.set('pulsar.txt')
-        root.destroy()
-
+    file.set('pulsar.txt')
+    root.destroy()
 
 
 def file_gun(file, root):
-        file.set('gosper-glider-gun.txt')
-        root.destroy()
-
+    file.set('gosper-glider-gun.txt')
+    root.destroy()
 
 
 def check_neighbours(board):
     dimensions = board.shape
-    #print(dimensions)
-    alive_cell = 'X'
-    dead_cell = '.'
+    # print(dimensions)
+    alive_cell = 1
+    dead_cell = 0
     rows, columns = dimensions[0], dimensions[1]
     temp_array = board.copy()
     for i in range(0, rows):
@@ -35,7 +32,7 @@ def check_neighbours(board):
             cell = board[i][j]
             nmb_of_neigh = 0
             try:
-                if board[i+1][j-1] == alive_cell: # checking all the surrounding cells
+                if board[i+1][j-1] == alive_cell:  # checking all the surrounding cells
                     nmb_of_neigh += 1
             except IndexError:
                 pass
@@ -74,8 +71,7 @@ def check_neighbours(board):
                     nmb_of_neigh += 1
             except IndexError:
                 pass
-            
-            
+
             if cell == alive_cell:
                 if nmb_of_neigh <= 1 or nmb_of_neigh >= 4:
                     temp_array[i][j] = dead_cell
@@ -84,43 +80,44 @@ def check_neighbours(board):
             elif cell == dead_cell:
                 if nmb_of_neigh == 3:
                     temp_array[i][j] = alive_cell
-            
+
     board = temp_array.copy()
     return board
 
+
 def game_window():
     root = Tk()
-    
+
     img_glider = ImageTk.PhotoImage(Image.open('glider.png'))
     img_glider_gun = ImageTk.PhotoImage(Image.open('glider_gun.png'))
     img_pulsar = ImageTk.PhotoImage(Image.open('pulsar.png'))
     glider_image = Label(root, image=img_glider)
     glider_gun_image = Label(root, image=img_glider_gun)
-    pulsar_image = Label(root, image=img_pulsar) 
-    
+    pulsar_image = Label(root, image=img_pulsar)
+
     file = StringVar()
-    header = Label(root, text='Which pattern do you want to see evolve?').grid(row=0, column=1, columnspan=2)
-    
-    
+    header = Label(root, text='Which pattern do you want to see evolve?').grid(
+        row=0, column=1, columnspan=2)
+
     # Making buttons
-    button_glider = Button(root, text='Glider', command=lambda: file_glider(file, root))
-    button_glider_gun = Button(root, text='Gosper_glider_gun', command=lambda: file_gun(file, root))
-    button_pulsar = Button(root, text='Pulsar', command= lambda: file_pulsar(file, root))
-    
-    
+    button_glider = Button(root, text='Glider',
+                           command=lambda: file_glider(file, root))
+    button_glider_gun = Button(
+        root, text='Gosper_glider_gun', command=lambda: file_gun(file, root))
+    button_pulsar = Button(root, text='Pulsar',
+                           command=lambda: file_pulsar(file, root))
+
     # Griding buttons
     button_glider.grid(row=1, column=0)
     button_glider_gun.grid(row=2, column=0)
     button_pulsar.grid(row=3, column=0)
-    
+
     glider_image.grid(row=1, column=1)
     glider_gun_image.grid(row=2, column=1)
     pulsar_image.grid(row=3, column=1)
 
-    # make some images to show the patterns and put them under the buttons!
-    
-    
-    root.mainloop()
-    
-    return file.get()
+    # make some images to show the patterns and put them under the buttons! Just above this comment
 
+    root.mainloop()
+
+    return file.get()
